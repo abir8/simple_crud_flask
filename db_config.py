22b27e94731596e0577e3dcb55e6134acb26d368
abir8 <engr.ashrafkhan01@gmail.com>
2020-02-11 13:22:24 +0600

    Initial Commit

--- db_config.py
+++ db_config.py
@@ -3,8 +3,8 @@ from app import app
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'ashraf'
-app.config['MYSQL_DATABASE_PASSWORD'] = '12345'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'flask_crud_api'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
